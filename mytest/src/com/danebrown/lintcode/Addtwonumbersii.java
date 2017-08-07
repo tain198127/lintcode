5b36fd7958223da3747a4f11b37ece4a22acc0f0
DaneBrown <tain198127@163.com>
2017-08-07 12:54:04 +0800

    增加答案

--- mytest/src/com/danebrown/lintcode/Addtwonumbersii.java
+++ mytest/src/com/danebrown/lintcode/Addtwonumbersii.java
@@ -18,6 +18,78 @@ package com.danebrown.lintcode;
 
 /**
  * Created by dane on 2017/8/7.
+ *
  */
+
+import java.util.Stack;
+
+/**
+ * source: http://www.lintcode.com/zh-cn/problem/add-two-numbers-ii/
+ */
+
 public class Addtwonumbersii {
+
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
+
+    /*
+     * @param l1: The first list.
+     * @param l2: The second list.
+     * @return: the sum list of l1 and l2.
+     */
+
+    public ListNode addLists2(ListNode l1, ListNode l2) {
+        // write your code here
+        Stack<Integer> st1 = new Stack<Integer>();
+        Stack<Integer> st2 = new Stack<Integer>();
+        Stack<Integer> result = new Stack<Integer>();
+
+        int append=0;//进位
+        while(l1 != null){
+            st1.push(l1.val);
+            //System.out.println(l1.val);
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            st2.push(l2.val);
+            //System.out.println(l2.val);
+            l2 = l2.next;
+        }
+
+        while(!(st1.isEmpty()&&st2.isEmpty()&&append == 0)){
+            int num1 = 0;
+            int num2 = 0;
+            if(!st1.isEmpty()){
+                num1 = st1.pop();
+            }
+            if(!st2.isEmpty()){
+                num2 = st2.pop();
+            }
+            int rst = num1+num2+append;
+
+            append = 0;
+            if(rst >= 10){
+                append = 1;//进位
+                rst = rst -10;
+            }
+            //System.out.println(rst);
+            result.push(rst);
+
+        }
+        ListNode ln = new ListNode(result.pop());
+        ListNode copy = ln;
+        while(!result.isEmpty()){
+            copy.next = new ListNode(result.pop());
+            copy = copy.next;
+        }
+        return ln;
+
+
+    }
 }
