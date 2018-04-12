e7b40cd4bc7543ffa09df788eeb2115cdb75a469
tain198127 <tain198127@163.com>
2018-04-12 23:54:55 +0800

    开始做 trillionmergesort

--- mytest/src/main/java/com/danebrown/lintcode/sort/TrillionMergeSort.java
+++ mytest/src/main/java/com/danebrown/lintcode/sort/TrillionMergeSort.java
@@ -1,8 +1,14 @@
 package com.danebrown.lintcode.sort;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.*;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.SplittableRandom;
+
+import static org.apache.commons.lang3.StringUtils.split;
 
 /**
  * 十亿级排序，采用归并算法
@@ -13,13 +19,25 @@ import java.util.SplittableRandom;
  * 4. 对每块进行 MERGE并写入到文件中
  */
 public class TrillionMergeSort {
+    /**
+     * 创建文件时，flush 的大小，表示每行有多少个数字
+     */
     private static int STACK_SIZE = 10000;
-    private static String SOURCE_DATA_NAME="SOURCE.txt";
+    /**
+     * 原始文件名称
+     */
+    private static String SOURCE_DATA_NAME = "SOURCE.txt";
+    private static String TEMP_FILE_NAME = "tmp-[%s].txt";
+    /**
+     * 拆分为小文件时，每个文件的行数
+     */
+    private static int SPLIT_FILE_LINE_SIZE = 3;
     SplittableRandom sr = new SplittableRandom();
 
     public static void main(String args[]) throws IOException {
         TrillionMergeSort sort = new TrillionMergeSort();
-        sort.generateTrillionData(10000000);
+//        sort.generateTrillionData(10000000);
+        sort.splitTrillionData();
     }
 
     /**
@@ -33,7 +51,7 @@ public class TrillionMergeSort {
          */
         File f = new File("SOURCE_DATA_NAME");
 
-        try(PrintWriter pw=new PrintWriter(new FileWriter(new File(SOURCE_DATA_NAME),true))){
+        try (PrintWriter pw = new PrintWriter(new FileWriter(new File(SOURCE_DATA_NAME), true))) {
             int j = 0;
             int k = 0;
             if (size < STACK_SIZE)
@@ -42,8 +60,8 @@ public class TrillionMergeSort {
 
                 for (int i = 0; i < STACK_SIZE; i++) {
                     int rdm = sr.nextInt(Integer.MAX_VALUE);
-                    pw.printf("%d,",rdm);
-                    if(i == STACK_SIZE-1){
+                    pw.printf("%d,", rdm);
+                    if (i == STACK_SIZE - 1) {
                         pw.println();
                     }
                     j++;
@@ -51,7 +69,7 @@ public class TrillionMergeSort {
                 }//end for
                 k++;
                 pw.flush();
-                System.out.printf("写入了%s次\n",k);
+                System.out.printf("写入了%s次\n", k);
             }//end whild
         }
 
@@ -59,10 +77,50 @@ public class TrillionMergeSort {
     }
 
     /**
-     * 将十亿数据分布到10000份中
+     * 将十亿数据分布到10000份中[完成]
      */
-    private void splitTrillionData() {
+    private void splitTrillionData() throws IOException {
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(SOURCE_DATA_NAME)))) {
+            String line = null;
+            int count = 0;
+            int times = 0;
+            PrintWriter br = null;
+            LinkedList<Integer> tempSort = null;
+            String fileName = null;
+            while ((line = reader.readLine()) != null) {
+                if (count == 0) {
+                    fileName = String.format(TEMP_FILE_NAME, times);
+                    br = new PrintWriter(new FileWriter(new File(fileName), false));
+                    tempSort = new LinkedList<>();
+                }
+                count++;
+                String[] tmpStr = line.split(",");
+                for (int i = 0; i < tmpStr.length; i++) {
+                    tempSort.add(Integer.parseInt(tmpStr[i]));
+                }
+                //br.write(line);
+
+                if (count > SPLIT_FILE_LINE_SIZE) {
+                    times++;
+                    count = 0;
+                    tempSort.sort(Comparator.comparingInt(o -> o));
+                    for (int i = 0; i < tempSort.size(); i++) {
+                        br.printf("%s,", tempSort.get(i));
+                        if ((i + 1) % STACK_SIZE == 0) {
+                            br.println();
+                        }
+                    }
+                    if (br != null) {
+
+                        br.flush();
+                        br.close();
+                        System.out.println(fileName + "写入完毕");
+                    }
+                }
+            }
+
+        }
     }
 
     /**
