25f0d955262015cc13c32f02cb744d6b5fd88c7f
dane brown <tain198127@163.com>
2021-11-26 15:41:07 +0800

    完成obj类型的spel

---
+++ mytest/src/main/java/com/danebrown/spel/CtxWrapper.java
@@ -0,0 +1,15 @@
+package com.danebrown.spel;
+
+import lombok.Data;
+
+/**
+ * Created by danebrown on 2021/11/26
+ * mail: tain198127@163.com
+ *
+ * @author danebrown
+ */
+@Data
+public class CtxWrapper {
+    private String wrapperName;
+    private String wrapperAge;
+}
--- mytest/src/main/java/com/danebrown/spel/SpelTest.java
+++ mytest/src/main/java/com/danebrown/spel/SpelTest.java
@@ -1,7 +1,6 @@
 package com.danebrown.spel;
 
 import com.danebrown.reactor.ReactorMain;
-import com.danebrown.spring.multitirecache.MultiTireCacheTest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
@@ -17,6 +16,9 @@ import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by danebrown on 2021/11/25
  * mail: tain198127@163.com
@@ -26,64 +28,72 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 @Log4j2
 @SpringBootApplication(scanBasePackages = {"com.danebrown.spel"})
 public class SpelTest implements CommandLineRunner, ApplicationListener<ApplicationReadyEvent> {
-    @Data
-    @AllArgsConstructor
-    public static class DataName{
-        private String name;
-        private String age;
-    }
-
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(ReactorMain.class);
         springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(SpelTest.class, args);
     }
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("startup");
     }
-
-public void invokeBySpel(DataName dataName,String property,String method){
-
-
-    EvaluationContext context = new StandardEvaluationContext(dataName);
-    context.setVariable("txHeader",dataName);
-//    String name = (String) exp.getValue(context);
+    //调用静态方法
+    public void invokeBySpel(DataName dataName, String property, String method) {
 
 
-//    System.out.println("使用SPEL获取DataName的name字段:"+name);
+        EvaluationContext context = new StandardEvaluationContext(dataName);
+        context.setVariable("txHeader", dataName);
 
 
-//    String invokeStatic =
-//            method+"(\""+name+"\")";
-
-    String invokeStatic1 =
-            method+"({#txHeader."+property+"})";
-
-    ExpressionParser methodCall = new SpelExpressionParser();
-    Expression methodExpr = methodCall.parseExpression(invokeStatic1);
-    methodExpr.getValue(context);
+        String invokeStatic1 = method + "({#txHeader." + property + "})";
+        System.out.println(invokeStatic1);
+        log.info("{}", invokeStatic1);
+        ExpressionParser methodCall = new SpelExpressionParser();
+        Expression methodExpr = methodCall.parseExpression(invokeStatic1);
+        methodExpr.getValue(context);
 
 
-//    ExpressionParser methodCall1 = new SpelExpressionParser();
-//    EvaluationContext context1 = new StandardEvaluationContext(dataName);
-//    Expression methodExpr1 = methodCall1.parseExpression("T(com.danebrown.spel.StaticClass)");
-//
-//    methodExpr1.setValue(property,name);
+    }
 
-}
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
 
-        DataName dataName = new DataName("张三","13");
-        String invokeStatic =
-                "T(com.danebrown.spel.StaticClass).setNameCtx";
-        invokeBySpel(dataName,"name",invokeStatic);
+        DataName dataName = new DataName("张三", "13");
+        String invokeStatic = "T(com.danebrown.spel.StaticClass).setNameCtx";
+        invokeBySpel(dataName, "name", invokeStatic);
 
-        String invokeStaticAge =
-                "T(com.danebrown.spel.StaticClass).setAgtCtx";
-        invokeBySpel(dataName,"age",invokeStaticAge);
+        String invokeStaticAge = "T(com.danebrown.spel.StaticClass).setAgtCtx";
+        invokeBySpel(dataName, "age", invokeStaticAge);
+
+        String invokectxName = "";
+        String invokeCtxAge = "";
+        HashMap<String,String> wrapper = new HashMap<>();
+        invokeByObj(dataName,wrapper,"name","wrapperName");
+        invokeByObj(dataName,wrapper,"age","wrapperAge");
 
 
+    }
+    //调用动态对象
+    private void invokeByObj(DataName dataName, Map<String,String> wrapper,
+                             String sourceProperties, String targetProperties){
+        EvaluationContext context = new StandardEvaluationContext(dataName);
+        context.setVariable("txHeader", dataName);
+        context.setVariable("ctx",wrapper);
+
+        String invokeSpel =
+                "#ctx['"+targetProperties+"']={#txHeader."+sourceProperties+
+                        "}";
+        ExpressionParser methodCall = new SpelExpressionParser();
+        Expression methodExpr = methodCall.parseExpression(invokeSpel);
+        methodExpr.getValue(context);
+        log.error("{}",wrapper);
+
+    }
+    @Data
+    @AllArgsConstructor
+    public static class DataName {
+        private String name;
+        private String age;
     }
 }
