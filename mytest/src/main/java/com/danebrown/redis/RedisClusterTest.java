ea7d8765f8cb4407b48ba5576fbbfb01d710bd4f
dane brown <tain198127@163.com>
2022-05-09 21:37:44 +0800

    redis客户端

---
+++ mytest/src/main/java/com/danebrown/redis/RedisClusterTest.java
@@ -0,0 +1,103 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.danebrown.redis;
+
+import io.lettuce.core.RedisChannelHandler;
+import io.lettuce.core.RedisConnectionStateAdapter;
+import io.lettuce.core.RedisConnectionStateListener;
+import io.lettuce.core.RedisFuture;
+import io.lettuce.core.api.push.PushMessage;
+import io.lettuce.core.api.sync.RedisStringCommands;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.async.RedisAdvancedClusterAsyncCommands;
+import io.lettuce.core.cluster.api.push.RedisClusterPushListener;
+import io.lettuce.core.cluster.api.reactive.RedisAdvancedClusterReactiveCommands;
+import io.lettuce.core.cluster.models.partitions.RedisClusterNode;
+import lombok.extern.log4j.Log4j2;
+import reactor.core.publisher.Mono;
+
+import java.net.SocketAddress;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Log4j2
+public class RedisClusterTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        RedisClusterClient client = RedisClusterClient.create("redis" +
+                "://127.0.0.1" +
+                ":7000");
+        StatefulRedisClusterConnection<String, String> connection = client.connect();
+        RedisConnectionStateListener listener =
+                new RedisConnectionStateListener(){
+                    @Override
+                    public void onRedisDisconnected(RedisChannelHandler<?, ?> connection) {
+                        log.info("onRedisDisconnected:{}",connection);
+                    }
+
+                    @Override
+                    public void onRedisExceptionCaught(RedisChannelHandler<?, ?> connection, Throwable cause) {
+                        log.info("onRedisExceptionCaught:{},exception:{}",
+                                connection,cause);
+                    }
+
+                    @Override
+                    public void onRedisConnected(RedisChannelHandler<?, ?> connection, SocketAddress socketAddress) {
+                        log.info("onRedisConnected:{}; sock:{}",connection,
+                                socketAddress);
+                    }
+                };
+
+        client.addListener(listener);
+        RedisClusterPushListener publishListener = new RedisClusterPushListener() {
+            @Override
+            public void onPushMessage(RedisClusterNode node, PushMessage message) {
+                log.info("node:{};content:{}",node.getNodeId(),message);
+            }
+        };
+        connection.addListener(publishListener);
+
+        log.info("date 通过keyslot的结果是:{}",
+                connection.sync().clusterKeyslot("date"));
+
+        RedisAdvancedClusterReactiveCommands<String,String> async =
+                connection.reactive();
+
+        log.info("访问前前的connection是:{}",connection);
+        async.set("key", "123")
+        .then(async.set("key1","234"))
+        .then(async.set("key2","567"))
+        .checkpoint();
+
+
+
+        Mono<String> value = async.get("key");
+
+        long dateKeySlot = async.clusterKeyslot("date").block();
+        log.info("date的keyslot是:{}",dateKeySlot);
+
+        log.info("key 是 {}， 值是:{}","key",value.block());
+        log.info("当前的connection是:{}",connection);
+        client.shutdown(1,10, TimeUnit.SECONDS);
+
+
+
+    }
+}
